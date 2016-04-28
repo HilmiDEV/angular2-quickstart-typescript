973dc379c8a8242a41ec17c24b40f4f546ad56c5
hbouallegue <hilmi.bouallegue@geomatys.com>
2016-04-28 11:09:07 +0200

    My First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Angular 2 QuickStart Application with TypeScript
+================================================
+* First install the dependecies of the project
+
+ **npm install
---
+++ src/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App with TypeScript</h1>'
+})
+/* the Class definition of our component that can be handle the view
+ by adding properties (attributs of the class) & behaviors (methods of the class)
+  */
+/*
+ Every Angular app has at least one root component,
+ conventionally named AppComponent, that hosts the client user experience.
+
+ */
+export class AppComponent { }
